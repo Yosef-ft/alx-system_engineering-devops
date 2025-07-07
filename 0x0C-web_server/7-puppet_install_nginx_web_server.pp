e05fa696a70ad645b6c9334d4f7fa8ce716088c1
Yosef <yosefetene02@gmail.com>
2025-07-08 01:36:03 +0300

    updated script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,2 +1,31 @@
 #!/usr/bin/env bash
 # Install nginx with puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => "Hello World!\n",
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/conf.d/redirect_me.conf':
+  ensure  => file,
+  content => "location = /redirect_me {\n    return 301 /;\n}\n",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
